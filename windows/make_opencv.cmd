760177c65f10791729ea538d0f1968910ca3e6a6
Joshua Zhang <cheungchih@gmail.com>
2017-09-05 19:35:18 -0700

    fix 7z

--- windows/make_opencv.cmd
+++ windows/make_opencv.cmd
@@ -7,8 +7,7 @@ mkdir deps
 cd deps
 echo Downloading opencv
 appveyor DownloadFile %OPENCV_URL% -FileName opencv.zip
-7z x opencv.zip -y -o opencv_src
-dir /b /a-d
+7z x opencv.zip -y -oopencv_src
 mkdir opencv_build
 cd opencv_build
 cmake ../opencv_src ^
