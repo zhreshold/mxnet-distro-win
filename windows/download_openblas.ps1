906b2fa5c48eba826d3ad72926f0aaed9cbbff18
Joshua Zhang <cheungchih@gmail.com>
2017-09-06 00:51:43 -0700

    test

--- windows/download_openblas.ps1
+++ windows/download_openblas.ps1
@@ -1,6 +1,7 @@
-$env:MXNET_OPENBLAS_FILE=openblas.7z
-$env:MXNET_OPENBLAS_DIR=openblas
-$env:MXNET_OPENBLAS_PKG=https://github.com/hjk41/MiscFiles/blob/master/openblas.7z?raw=true
+$env:MXNET_OPENBLAS_FILE = "openblas.7z"
+$env:MXNET_OPENBLAS_DIR = "openblas"
+$env:MXNET_OPENBLAS_PKG = "https://github.com/hjk41/MiscFiles/blob/master/openblas.7z?raw=true"
+
 if (!(Test-Path ${env:MXNET_OPENBLAS_FILE})) {
     echo "Downloading openblas from ${env:MXNET_OPENBLAS_PKG} ..."
     appveyor DownloadFile "${env:MXNET_OPENBLAS_PKG}" -FileName ${env:MXNET_OPENBLAS_FILE} -Timeout 1200000
