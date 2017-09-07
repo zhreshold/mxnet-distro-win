d0d4bbdd32ef707fad83f2e1127c355354933bd4
Joshua Zhang <cheungchih@gmail.com>
2017-09-07 11:25:02 -0700

    add cuda

--- windows/make_cuda.cmd
+++ windows/make_cuda.cmd
@@ -1,4 +1,7 @@
 @echo off
+cd C:\
+mkdir deps
+cd deps
 echo Downloading CUDA toolkit 8
 appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe -FileName cuda_8.0.44_windows.exe
 echo Installing CUDA toolkit 8
@@ -32,3 +35,4 @@ exit /B 1
 set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\libnvvp;%PATH%
 
 nvcc -V
+cd %APPVEYOR_BUILD_FOLDER%
