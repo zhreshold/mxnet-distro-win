6f4b5f2e456c03b684836c03a925c6e337b0d156
Joshua Zhang <cheungchih@gmail.com>
2017-09-08 23:46:48 -0700

    debug

--- windows/init.cmd
+++ windows/init.cmd
@@ -1,14 +1,14 @@
 REM "Init environment"
 
 REM "32/64 bit visual studio"
-IF %TARGET_ARCH% == 32 (
+IF %TARGET_ARCH% == "32" (
   echo "Setting build target 32bit"
   call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
   set CMAKE_BUILD_PLATFORM="Visual Studio 12 2013"
   set PYTHON_EXE=C:\%PYTHON%\python
   set PIP_EXE=C:\%PYTHON%\Scripts\pip
 )
-IF %TARGET_ARCH% == 64 (
+IF %TARGET_ARCH% == "64" (
   echo "Setting build target 64bit"
   call "C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\SetEnv.cmd" /x64
   call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
