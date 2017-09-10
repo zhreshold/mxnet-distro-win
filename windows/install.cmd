5af30990b48f4f7d385e74846ccde1100fa0313f
Joshua Zhang <cheungchih@gmail.com>
2017-09-10 16:07:46 -0700

    debug

--- windows/install.cmd
+++ windows/install.cmd
@@ -8,4 +8,7 @@ REM pip install
 call %PIP_EXE% install pypandoc nose wheel twine
 call %PYTHON_EXE% -c "import pypandoc"
 SET NOSE_EXE=%PYTHON_DIR%\Scripts\nosetests
+SET TWINE_EXE=%PYTHON_DIR%\Scripts\twine
+dir %PYTHON_DIR%\Scripts\
 call %NOSE_EXE% --version
+call %TWINE_EXE% --version
