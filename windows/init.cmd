43b0f07d417165d3561135e0b69f6ac22526f99e
Joshua Zhang <cheungchih@gmail.com>
2017-09-10 01:15:53 -0700

    fix2

--- windows/init.cmd
+++ windows/init.cmd
@@ -11,8 +11,8 @@ IF %TARGET_ARCH% == 32 (
 )
 IF %TARGET_ARCH% == 64 (
   echo "Setting build target 64bit"
-  call "C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\SetEnv.cmd" /x64
-  call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+  REM call "C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\SetEnv.cmd" /x64
+  REM call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
   set CMAKE_BUILD_PLATFORM="Visual Studio 12 2013 Win64"
   set PYTHON_DIR=C:\%PYTHON%-x64
   set PYTHON_EXE=C:\%PYTHON%-x64\python
