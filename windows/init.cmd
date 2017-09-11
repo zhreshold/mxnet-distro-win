5bbd118bd8e80e69e07b8d47e07e50844b7e4e1d
Joshua Zhang <cheungchih@gmail.com>
2017-09-10 19:28:02 -0700

    pypirc

--- windows/init.cmd
+++ windows/init.cmd
@@ -24,3 +24,31 @@ call %PYTHON_EXE% --version
 call %PIP_EXE% --version
 REM "Check supported wheels"
 call %PYTHON_EXE% -c "import pip; print(pip.pep425tags.get_supported())"
+
+REM "Write secure password to pypirc"
+echo [distutils] # this tells distutils what package indexes you can push to > %USERPROFILE%\.pypirc
+echo index-servers = >> %USERPROFILE%\.pypirc
+echo     pypi >> %USERPROFILE%\.pypirc
+echo     pypitest >> %USERPROFILE%\.pypirc
+echo     legacy >> %USERPROFILE%\.pypirc
+echo   >> %USERPROFILE%\.pypirc
+echo [pypi] >> %USERPROFILE%\.pypirc
+echo repository: https://pypi.python.org/pypi >> %USERPROFILE%\.pypirc
+echo username: %PYPI_USERNAME% >> %USERPROFILE%\.pypirc
+echo password: %PYPI_PASSWORD% >> %USERPROFILE%\.pypirc
+echo   >> %USERPROFILE%\.pypirc
+echo [pypitest] >> %USERPROFILE%\.pypirc
+echo repository: https://pypi.python.org/pypi >> %USERPROFILE%\.pypirc
+echo username: %PYPI_USERNAME% >> %USERPROFILE%\.pypirc
+echo password: %PYPI_PASSWORD% >> %USERPROFILE%\.pypirc
+echo   >> %USERPROFILE%\.pypirc
+echo [test] >> %USERPROFILE%\.pypirc
+echo repository: https://pypi.python.org/pypi >> %USERPROFILE%\.pypirc
+echo username: %PYPI_USERNAME% >> %USERPROFILE%\.pypirc
+echo password: %PYPI_PASSWORD% >> %USERPROFILE%\.pypirc
+echo   >> %USERPROFILE%\.pypirc
+echo [server-login] >> %USERPROFILE%\.pypirc
+echo repository: https://pypi.python.org/pypi >> %USERPROFILE%\.pypirc
+echo username: %PYPI_USERNAME% >> %USERPROFILE%\.pypirc
+echo password: %PYPI_PASSWORD% >> %USERPROFILE%\.pypirc
+dir %USERPROFILE%
